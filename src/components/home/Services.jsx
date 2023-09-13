34d422412822b3af3d91aac9df2de77b1d7ca0ee
alif33 <alifhasan332@gmail.com>
2023-09-13 11:57:09 +0600

    project init

---
+++ src/components/home/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Services = () =>{
+    return(
+       <div className="bg-[#FDF4FF]">
+            <div className="flex justify-between">
+               <h1 className="text-4xl font-black">{"Our services are customized to perfectly meet your pet's unique needs."}</h1> 
+               <span>
+                    <h5>See all</h5>
+               </span>
+            </div>
+
+       </div> 
+    )
+}
+export default Services;
