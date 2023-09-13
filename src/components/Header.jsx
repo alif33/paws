908d46b5459e9d8dabbba914df9be97587e035ef
Mridul Das <mriduldas0325@gmail.com>
2023-09-13 13:21:11 +0600

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import Image from "next/image";
 
-const Header = () =>{
-    return(
-        <div className="flex">
-            
-        </div>
-    )
-}
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center max-w-[1300px] mx-auto mt-[18px]">
+      <div className="flex justify-center items-center gap-[14px]">
+        <Image src="/images/logo.png" width={74} height={74} alt="logo" />
+        <Image
+          src="/images/PAWS.PLACE.png"
+          width={191}
+          height={24}
+          alt="brand_name"
+        />
+      </div>
+      <div className="">
+        <ul className="flex justify-center items-center gap-[25px] text-[#3D3D3D] text-[16px] font-medium">
+          <li className="cursor-pointer">SERVICES</li>
+          <li className="cursor-pointer">GALLERY</li>
+          <li className="cursor-pointer">ABOUT US</li>
+          <li className="bg-[#6F0C88] text-white py-[14px] px-[28px] rounded-[7px] cursor-pointer">
+            BOOK NOW
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 export default Header;
--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-
-const Hero = () =>{
-    return(
-        <div className="flex">
-            <div className="grid grid-cols-12">
-                <div className="col-span-7">
-                    <h1 className="text-5xl font-black">{"Stress-Free Dog Grooming: Where Your Pet's Happiness Matters."}</h1>
-                    <h6 className="text-sm font-normal">{"Experience grooming like never before at our pet spa. We prioritize your dog's comfort, providing a soothing and stress-free grooming experience. Your furry friend deserves the best, and we're here to pamper them with care and love."}</h6>
-                    <button className="bg-[#6F0C88] text-white py-2 px-3 rounded">BOOK NOW</button>
-                </div>
-            </div>
+const Hero = () => {
+  return (
+    <div className="flex">
+      <div className="grid grid-cols-12">
+        <div className="col-span-7">
+          <h1 className="text-5xl font-black">
+            {"Stress-Free Dog Grooming: Where Your Pet's Happiness Matters."}
+          </h1>
+          <h6 className="text-sm font-normal">
+            {
+              "Experience grooming like never before at our pet spa. We prioritize your dog's comfort, providing a soothing and stress-free grooming experience. Your furry friend deserves the best, and we're here to pamper them with care and love."
+            }
+          </h6>
+          <button className="bg-[#6F0C88] text-white py-2 px-3 rounded">
+            BOOK NOW
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default Hero;
