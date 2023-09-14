69560bad933e820252eaf562fb08d3ddf6700a86
alif33 <alifhasan332@gmail.com>
2023-09-14 20:53:41 +0600

    fiex header layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="grid grid-cols-2 justify-between items-center max-w-[1300px] mx-auto mt-[18px]">
+    <div className="grid grid-cols-2 justify-between items-center max-w-[1300px] px-10 mx-auto mt-[18px]">
       <div className="col-span-2 sm:col-span-2 md:col-span-1">
         <div className="flex justify-center sm:justify-center md:justify-normal items-center gap-[14px]">
           <Image src="/images/logo.svg" width={74} height={74} alt="logo" />
@@ -16,7 +16,7 @@ const Header = () => {
         </div>
       </div>
       <div className="col-span-2 sm:col-span-2 md:col-span-1">
-        <ul className="flex justify-center items-center gap-[25px] text-[#3D3D3D] text-[16px] font-medium">
+        <ul className="flex justify-center sm:justify-center md:justify-end items-center gap-[25px] text-[#3D3D3D] text-[16px] font-medium">
           <li className="cursor-pointer"><Link href="/#services">SERVICES</Link></li>
           <li className="cursor-pointer"><Link href="/#gallery">GALLERY</Link></li>
           <li className="cursor-pointer"><Link href="/#about-us">ABOUT US</Link></li>
