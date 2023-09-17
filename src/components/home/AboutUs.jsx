7c829d077c7e0d7c289b6a9dcfaa08715f436fbb
Mridul Das <mriduldas0325@gmail.com>
2023-09-17 21:16:01 +0600

    footer align

--- src/components/home/AboutUs.jsx
+++ src/components/home/AboutUs.jsx
@@ -28,7 +28,7 @@ export default function AboutUs() {
   return (
     <div
       id="about-us"
-      className="bg-[url('/images/shapes/BgAboutUs.svg')] bg-cover bg-no-repeat pt-[7rem] pb-11 lg:pb-16 px-2 sm:px-2 md:px-[100px] mt-[-7rem]"
+      className="bg-[url('/images/shapes/BgAboutUs.svg')] bg-cover bg-no-repeat pt-[7rem] pb-24 lg:pb-32 px-2 sm:px-2 md:px-[100px] mt-[-7rem]"
     >
       <h1 className="text-[#6F0C88] text-[20px] lg:text-[40px] font-black text-center pt-[50px] pb-[25px] lg:pb-[50px]">
         What our clients tell about us
@@ -164,11 +164,11 @@ export default function AboutUs() {
           <BsChevronRight size={28} />
         </button>
       </div>
-      <div className="flex justify-center items-center gap-[10px] pb-[80px]">
+      {/* <div className="flex justify-center items-center gap-[10px] pb-[80px]">
         <div className="min-w-[23px] min-h-[23px] bg-[#D9D9D9] rounded-full"></div>
         <div className="min-w-[23px] min-h-[23px] bg-[#6F0C88] rounded-full"></div>
         <div className="min-w-[23px] min-h-[23px] bg-[#D9D9D9] rounded-full"></div>
-      </div>
+      </div> */}
     </div>
   );
 }
